6d3dee2f6d61fb35b50107d3496347454d3e1cdd
xani-dev <xani.gn@gmail.com>
2023-07-10 10:59:44 -0400

    Quiz Submission

--- name_checker.py
+++ name_checker.py
@@ -1,8 +1,25 @@
 from ValidationException import ValidationException
 
 def validate_file(file_path):
-    pass #TODO: Only add code inside this function.
+    print('Hi, Opening the file: ', file_path)
+    
+    with open(file_path, 'r') as file_to_validate:
+        for line_number, line in enumerate(file_to_validate, start = 1):
+            if line_number == 1:
+                continue
+            first_name = line.partition(',')[0]
+            
 
+            
+            try: 
+               if first_name.isalpha():
+                   continue
+               else:
+                    print('Invalid First Name:',first_name)
+            except ValueError:
+                raise ValidationException(f'Invalid Name: {first_name}')
+            
+            
 
 def test():
     try:
